02feafb9725762423c036de482d2a8e6a281feb2
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-04-26 11:21:59 +0530

    push1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  let relativesList = ["Srikanth", "Rambabu", "Jaya Chandra", "Vishnu"];
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol>
+        {relativesList.map((ele, idx) => (
+          <li key={`relativeListItem${idx + 1}`}>{ele}</li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
